1997a9dbeaa9f5c2a6d7e178212cc3d1f3d4cb61
Alex Rechtin <alexrechtin@gmail.com>
2021-10-28 12:30:32 +0200

    neue funktion hinzugefügt

--- formularFunctions.inc.php
+++ formularFunctions.inc.php
@@ -5,10 +5,27 @@
         }
         $stringFormular = "<select name='".$name."'>";
         for ($i=$ersterWert; $i <= $letzterWert ; $i+=$schrittweite) { 
-            $stringFormular.="\n<option value=$i >$i</option>"; 
+            $stringFormular.="\n<option value='".$i."'>$i</option>"; 
         }
         $stringFormular.="\n</select>";
 
         return $stringFormular;
+    }
+
+    function auswahllisteArray($name, $datenArray, $selected=""){
+        if (!is_string($name) || !is_array($datenArray) || !is_string($selected)) {
+            return false;
+        }
+
+        $stringFormular = "<select name='".$name."'>";
+        foreach ($datenArray as $value) {
+            if ($value === $selected) {
+                $stringFormular .= "\n<option value='".$value."'>selected>'".$value."'</option> </select>"
+            }
+            else{
+                $stringFormular .= "\n<option value='".$value."' >$value</option>";
+            }
+            
+        }
     }
 ?>
--- formularFunctionsTest.php
+++ formularFunctionsTest.php
@@ -6,6 +6,9 @@
             echo auswahlliste("test",2,6,1); 
             if(false === auswahlliste("test",2,9,"test") ) 
             echo "<br> Falscher Aufruf"; 
+
+            $datenArray=array("Erna Meier","Otto Meier","Oskar Schmidt"); 
+            echo auswahllisteArray("test",$datenArray, "Oskar Schmidt"); 
         ?>
     </body>
 </html>
